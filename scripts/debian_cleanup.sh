ecfa51ba34f4f624d0782eb707eb193b6554a744
Charles Sanquer <charles.sanquer@gmail.com>
2016-12-05 11:20:47 +0100

    update docker packer template

---
+++ scripts/debian_cleanup.sh
@@ -0,0 +1,63 @@
+#!/bin/bash -eux
+
+# Clean up
+apt-get -y --purge autoremove
+apt-get -y purge $(dpkg --list |grep '^rc' |awk '{print $2}')
+apt-get -y purge $(dpkg --list |egrep 'linux-image-[0-9]' |awk '{print $3,$2}' |sort -nr |tail -n +2 |grep -v $(uname -r) |awk '{ print $2}')
+apt-get -y clean
+
+# delete linux source
+dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
+
+# delete development packages
+dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
+
+# delete compilers and other development tools
+# apt-get -y purge cpp gcc g++
+
+# delete X11 libraries
+apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
+
+# delete obsolete networking
+apt-get -y purge ppp pppconfig pppoeconf
+
+# delete oddities
+apt-get -y purge popularity-contest
+
+apt autoremove
+apt update
+
+# Cleanup Virtualbox
+rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
+
+# Remove unused locales
+# rm -rf /usr/share/locale/{af,am,ar,as,ast,az,bal,be,bg,bn,bn_IN,br,bs,byn,ca,cr,cs,csb,cy,da,de,de_AT,dz,el,en_AU,en_CA,eo,es,et,et_EE,eu,fa,fi,fo,fur,ga,gez,gl,gu,haw,he,hi,hr,hu,hy,id,is,it,ja,ka,kk,km,kn,ko,kok,ku,ky,lg,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,nb,ne,nl,nn,no,nso,oc,or,pa,pl,ps,pt,pt_BR,qu,ro,ru,rw,si,sk,sl,so,sq,sr,sr*latin,sv,sw,ta,te,th,ti,tig,tk,tl,tr,tt,ur,urd,ve,vi,wa,wal,wo,xh,zh,zh_HK,zh_CN,zh_TW,zu}
+
+# Remove docs
+rm -rf /usr/share/doc
+
+# Remove files from cache
+find /var/cache -type f -delete -print
+
+# Remove guest addition source
+rm -rf /usr/src/virtualbox-ose-guest*
+rm -rf /usr/src/vboxguest*
+
+# Remove history file
+unset HISTFILE
+if [ -f ~/.bash_history ]; then
+    rm ~/.bash_history
+fi
+
+if [ -f /home/vagrant/.bash_history ]; then
+    rm /home/vagrant/.bash_history
+fi
+
+# Zero out the free space to save space in the final image:
+echo "Zeroing device to make space..."
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+# Block until the empty file has been removed, otherwise, Packer
+# will try to kill the box while the disk is still full and that's bad
+sync
